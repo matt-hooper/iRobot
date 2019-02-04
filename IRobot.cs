c074ed68cef810145808cd06bbf13be68e939d38
Matthew Hooper <mhooper@scottlogic.co.uk>
2019-02-04 09:49:21 +0000

    resolve conflicts

--- IRobot.cs
+++ IRobot.cs
@@ -1,16 +1,8 @@
 namespace iRobot
 {
-<<<<<<< HEAD
     public interface IRobot {
         void MoveForward(int numberOfSteps);
         void Rotate(double angle);
         void Beep(int numberOfBeeps , double volume);
-=======
-    public interface IRobot 
-    {
-        void MoveForward(int steps);
-        void Rotate(float angle);
-        void Beep(int noOfBeeps, int volume);
->>>>>>> b929c9e558fee1a1fb0f18e21a8e67311cf18649
     }
 }
